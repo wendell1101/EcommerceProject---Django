c2efb85b4cd884c49f2d6436f8279b897c829c98
wendell1101 <wendellchansuazo11@gmail.com>
2020-10-11 20:08:07 +0800

    change decimalfield

--- products/models.py
+++ products/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     title               = models.CharField(max_length=100)
     slug                = models.SlugField()
     quantity            = models.IntegerField(default=1)
-    price               = models.DecimalField(max_digits=1000000, decimal_places=2)
+    price               = models.DecimalField(max_digits=1000, decimal_places=2)
     discount_percent    = models.DecimalField(max_digits=3,decimal_places=2, blank=True, default=0)
     category            = models.CharField(choices=CATEGORY_CHOICES, max_length=100, default="none")
     label               = models.CharField(choices=LABEL_CHOICES, max_length=100, default="none")
